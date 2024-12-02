6e2f223842895396dba28b11563208ae9f0474d3
Dilankl30 <lucerodk7@gmail.com>
2024-12-01 20:29:56 -0500

    Finish

--- src/main/java/ec/edu/espoch/practicacarros/PracticaCarros.java
+++ src/main/java/ec/edu/espoch/practicacarros/PracticaCarros.java
@@ -1,8 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ec.edu.espoch.practicacarros;
+
+import Automoviles.Automoviles;
+import Enumeraciones.CarTypes;
+import Enumeraciones.FuelTypes;
 
 /**
  *
@@ -11,7 +14,32 @@ package ec.edu.espoch.practicacarros;
 public class PracticaCarros {
 
     public static void main(String[] args) {
-        
-        
+        Automoviles carros = new Automoviles("Mazda", 2024, 25.5, FuelTypes.Diesel, CarTypes.Family, 6, 5, 200, "white", 0);
+
+        carros.showAttributes();
+
+        carros.accelerate(100);
+        System.out.println(" ");
+        System.out.println("Accelerating");
+        System.out.println("Current Speed: " + carros.getCurrentSpeed() + " Km/h");
+
+        carros.accelerate(20);
+        System.out.println(" ");
+        System.out.println("Accelerating + ");
+        System.out.println("Current Speed: " + carros.getCurrentSpeed() + " Km/h");
+
+        carros.decelerate(50);
+        System.out.println(" ");
+        System.out.println("Decelerating");
+        System.out.println("Current Speed: " + carros.getCurrentSpeed() + " Km/h");
+
+        carros.time(150);
+        System.out.println(" ");
+        System.out.println("The estimated arrival time is:" + carros.getTime1());
+
+        System.out.println(" ");
+        System.out.println("Braking");
+        carros.curb();
     }
+
 }
